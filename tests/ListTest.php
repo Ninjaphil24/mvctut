2e99bcfcfdfcfd9b25d71cf93145903946e99eb1
Ninjaphil24 <mphilippos@hotmail.com>
2023-10-22 12:29:59 +0300

    Add ListTest & Repair AutoIncrement ID

---
+++ tests/ListTest.php
@@ -0,0 +1,115 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ListTest extends TestCase
+{
+    private $con;
+
+    protected function setUp(): void
+    {
+        $this->con = new mysqli("localhost", "mphil", "", "mvctut");
+        $this->idNumberSetup();
+        $this->createJohnSmith();
+    }
+
+    private $max_id;
+    private function idNumberSetup()
+    {
+        $sql = "SELECT MAX(id) as max_id  FROM users";
+        $result = $this->con->query($sql);
+        $row = $result->fetch_assoc();
+        $this->max_id = $row['max_id'];
+    }
+
+    private function createJohnSmith()
+    {
+        $_POST = [
+            'first_name' => 'John',
+            'last_name' => 'Smith',
+            'email' => 'john@smith.com',
+            'submit' => 'Submit'
+        ];
+
+        $first_name = $_POST['first_name'];
+        $last_name = $_POST['last_name'];
+        $email = $_POST['email'];
+
+        $query = "INSERT INTO users (first_name, last_name, email) VALUES (?, ?, ?)";
+
+        $statement = $this->con->prepare($query);
+
+        $statement->bind_param(
+            "sss",
+            $first_name,
+            $last_name,
+            $email
+        );
+        $statement->execute();
+    }
+
+    public function testListMethodResult()
+    {
+        $sql = "SELECT * FROM users";
+        $result = $this->con->query($sql);
+        $this->assertInstanceOf('mysqli_result',$result);
+    }
+
+    public function testListMethodContentSql()
+    {
+        $sql = "SELECT * FROM users ORDER BY id desc";
+        $result = $this->con->query($sql);
+        $row = $result->fetch_assoc();
+        $this->assertEquals('John',$row['first_name']);
+    }
+    
+    public function testListMethodContentWhile()
+    {
+        $sql = "SELECT * FROM users";
+        $result = $this->con->query($sql);
+        $lastrow=NULL;
+        while($row = $result->fetch_assoc()){
+            $lastrow = $row['first_name'];
+        };
+        $this->assertEquals('John',$lastrow);
+    }
+    
+    public function testListMethodContentFetchAllForEach()
+    {
+        $sql = "SELECT * FROM users";
+        $result = $this->con->query($sql);        
+        $rows = $result->fetch_all(MYSQLI_ASSOC);
+        $lastrow=NULL;
+        foreach ($rows as $row)$lastrow=$row['first_name'];
+        $this->assertEquals('John',$lastrow);
+    }
+    
+    public function testListMethodContentFetchAllEnd()
+    {
+        $sql = "SELECT * FROM users";
+        $result = $this->con->query($sql);        
+        $rows = $result->fetch_all(MYSQLI_ASSOC);
+        $last=end($rows);
+        $this->assertEquals('John',$last['first_name']);
+    }
+
+    public function deleteRow()
+    {
+        $query = "DELETE FROM users WHERE email = 'john@smith.com'";
+        $this->con->query($query);
+    }
+
+    private function idNumberTearDown()
+    {
+        $newMax = $this->max_id + 1;
+        $sql = "ALTER TABLE users AUTO_INCREMENT = $newMax"; 
+        $result = $this->con->query($sql);
+    }
+
+    protected function tearDown(): void
+    {
+        $this->deleteRow();
+        $this->idNumberTearDown();
+        $this->con->close();
+    }
+}
