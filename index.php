478a584687ce4d11d62722cf572355fee23fcba1
Ninjaphil24 <mphilippos@hotmail.com>
2024-01-31 18:13:42 +0200

    Push Notifications 1

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 require "vendor/autoload.php";
+
 use RouterSpace\Routes;
+
 require_once('env.php');
 require_once('mysqlconnect.php');
 ?>
@@ -13,6 +15,15 @@ require_once('mysqlconnect.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/index.css">
     <title>Vanilla Form</title>
+    <script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
+    <script>
+        window.OneSignalDeferred = window.OneSignalDeferred || [];
+        OneSignalDeferred.push(function(OneSignal) {
+            OneSignal.init({
+                appId: "f06ab8e3-2ebd-4be5-9400-7326cf760973",
+            });
+        });
+    </script>
 </head>
 
 <body>
@@ -20,24 +31,24 @@ require_once('mysqlconnect.php');
     $router = new Routes;
     $router->dispatch();
     ?>
-<select id="bgcolorpick">
-    <option value="">Original</option>
-    <option value="red">Red</option>
-    <option value="green">Green</option>
-    <option value="purple">Purple</option>
-</select>
+    <select id="bgcolorpick">
+        <option value="">Original</option>
+        <option value="red">Red</option>
+        <option value="green">Green</option>
+        <option value="purple">Purple</option>
+    </select>
 
-<script>
-document.addEventListener("DOMContentLoaded", ()=>{
-document.body.style.backgroundColor = localStorage.getItem("bgcolor")
-document.getElementById('bgcolorpick').value = localStorage.getItem('bgcolor')
-})
-document.getElementById('bgcolorpick').addEventListener('change', function() {
-    var color = document.getElementById("bgcolorpick").value
-    localStorage.setItem("bgcolor",color)
-    document.body.style.backgroundColor = this.value
-})
-</script>
+    <script>
+        document.addEventListener("DOMContentLoaded", () => {
+            document.body.style.backgroundColor = localStorage.getItem("bgcolor")
+            document.getElementById('bgcolorpick').value = localStorage.getItem('bgcolor')
+        })
+        document.getElementById('bgcolorpick').addEventListener('change', function() {
+            var color = document.getElementById("bgcolorpick").value
+            localStorage.setItem("bgcolor", color)
+            document.body.style.backgroundColor = this.value
+        })
+    </script>
 </body>
 
 </html>
