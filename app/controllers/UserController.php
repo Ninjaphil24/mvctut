a5a6ce5fa576010839b8621e230a4ab53b8774e7
Ninjaphil24 <mphilippos@hotmail.com>
2024-02-01 18:19:12 +0200

    Push Notifications Complete

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -3,6 +3,20 @@
 namespace UserControllerSpace;
 
 use UserModelNamespace\UserModel;
+use DateTime;
+use onesignal\client\api\DefaultApi;
+use onesignal\client\Configuration;
+use onesignal\client\model\GetNotificationRequestBody;
+use onesignal\client\model\Notification;
+use onesignal\client\model\StringMap;
+use onesignal\client\model\Player;
+use onesignal\client\model\UpdatePlayerTagsRequestBody;
+use onesignal\client\model\ExportPlayersRequestBody;
+use onesignal\client\model\Segment;
+use onesignal\client\model\FilterExpressions;
+use PHPUnit\Framework\TestCase;
+use GuzzleHttp;
+
 
 class UserController
 {
@@ -10,7 +24,7 @@ class UserController
     public function __construct()
     {
         global $con;
-        $this->con = $con;        
+        $this->con = $con;
     }
     function home()
     {
@@ -28,34 +42,69 @@ class UserController
             $email = $_POST['email'];
             $store = new UserModel;
             $errorMsg = $store->createUser($this->con, $first_name, $last_name, $email);
-            if ($errorMsg==="") require_once('app/views/success.php');
-            else require_once('app/views/home.php');
-            setcookie("FirstName",$first_name,time()+10,"/");
+            if ($errorMsg === "") {
+                // Enter push notification function here
+                $this->sendPush($first_name);
+                require_once('app/views/success.php');
+            } else require_once('app/views/home.php');
+            setcookie("FirstName", $first_name, time() + 20, "/");
         }
     }
+
+    public function sendPush($first_name)
+    {
+        $config = Configuration::getDefaultConfiguration()
+            ->setAppKeyToken(APP_KEY_TOKEN);
+
+        $apiInstance = new DefaultApi(
+            new GuzzleHttp\Client(),
+            $config
+        );
+        $notification = $this->createNotification('User: ' . $first_name . ' has subscribed!');
+       
+        $result = $apiInstance->createNotification($notification);
+        error_log(print_r($result, true));
+    }
+
+    function createNotification($enContent): Notification
+    {
+        $content = new StringMap();
+        $content->setEn($enContent);
+
+        $notification = new Notification();
+        $notification->setAppId(APP_ID);
+        $notification->setContents($content);
+        $notification->setIncludedSegments(['All']);
+
+        return $notification;
+    }
+
+
     /**
      * @param callable Parameter will lead code to the next step, if middleware conditions are met
      * @return mixed Returning a possible function or string.
      */
-    function cookieMiddleware($next){
-        return isset($_COOKIE["FirstName"]) ? call_user_func($next) : "No Cookie Set! ";            
+    function cookieMiddleware($next)
+    {
+        return isset($_COOKIE["FirstName"]) ? call_user_func($next) : "No Cookie Set! ";
     }
-    
+
     /**
      * Defines the function to be executed if middleware conditions are met
      * Example, return include('XYZ')
      */
-    function echoCookie() {
+    function echoCookie()
+    {
         echo "Hello " . $_COOKIE["FirstName"] . "  ";
     }
-    
+
     /**
      * Middleware Controller
      */
-    function executeFilter() {
-        $response = $this->cookieMiddleware([$this,'echoCookie']);
+    function executeFilter()
+    {
+        $response = $this->cookieMiddleware([$this, 'echoCookie']);
         // If a string is returned it's an error.  
         echo is_string($response) ? $response : $response;
     }
 }
-
